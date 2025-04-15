22d18cb4d6aba25764a939479f59220a1990f8c6
AlysonSouza61 <alyson_anapaz61@hotmail.com>
2025-04-15 12:33:45 -0300

    Update home.py

--- home.py
+++ home.py
@@ -190,11 +190,39 @@ if uploaded_file:
     )
     st.title("Média do MC por Iniciador")
     st.plotly_chart(fig)
+
+    # Gráfico 2
+
+    df_grouped_iniciado = df.groupby("Iniciador")["MC"].sum().reset_index()
+
+    df_grouped_iniciado = df_grouped_iniciado.sort_values(by="MC", ascending=False)
+
+    # Formatar os valores de MC como moeda BR (R$)
+    df_grouped_iniciado['MC_formatted'] = df_grouped_iniciado['MC'].apply(lambda x: f'R${x:,.2f}')
+
+    # Inicializando o app Dash
+    app = dash.Dash(__name__)
+
+    # Criando o gráfico com rótulos de dados
+    fig = px.bar(df_grouped_iniciado, x='Iniciador', y='MC', title='Acumulado do MC por Iniciador')
+
+    # Adicionando rótulos de dados no gráfico com formatação de moeda BR
+    fig.update_traces(text=df_grouped_iniciado['MC_formatted'], textposition='outside')
+
+    # Configuração do Streamlit
+    # Ajustando o layout (tamanho do gráfico)
+    fig.update_layout(
+        width=1000,  # Largura do gráfico
+        height=600,  # Altura do gráfico
+        margin=dict(t=50, b=100, l=50, r=50),  # Margens para evitar corte
+    )
+    st.title("Acumulado do MC por Iniciador")
+    st.plotly_chart(fig)
         
     # Exibir dataframe filtrado
     st.dataframe(df)
 
-    # Gráfico 2
+    # Gráfico 3
 
     df_grouped_iniciado = df.groupby("Descrição Defeito")["SN"].mean().reset_index()
 
