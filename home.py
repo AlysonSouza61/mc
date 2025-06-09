c57328cabc35929d63ef89da68dfb3bc3d4f9e2d
AlysonSouza61 <alyson_anapaz61@hotmail.com>
2025-06-09 09:32:17 -0300

    Update home.py

--- home.py
+++ home.py
@@ -48,7 +48,7 @@ if uploaded_file:
         df = pd.read_excel(uploaded_file, sheet_name='Dados Gerais RAC - Atualizado')
 
         # Lista de descrições de defeito a excluir
-    defeitos_excluidos = ["Material De Teste", "Devolução Comercial"]
+    defeitos_excluidos = ["Material De Teste", "Devolução Comercial", "Atraso na entrega", "Material Molhado", "Pedido Divergente", "Sentido De Embobinameto"]
     
     # Filtra removendo essas descrições da coluna
     df = df[~df['Descrição Defeito'].fillna('').str.strip().str.lower().isin([d.lower() for d in defeitos_excluidos])]
