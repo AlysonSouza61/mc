e92e7029e0b99eaf51e4f6b7b3f9eab2482bac02
AlysonSouza61 <alyson_anapaz61@hotmail.com>
2025-09-01 13:51:40 -0300

    Update home.py

--- home.py
+++ home.py
@@ -67,7 +67,7 @@ if uploaded_file:
     # Fator 4: (df['SN'] * 0.01 + 0.99)
     # Fator 5: (df['SN'] * 0.005 + 0.995)
     df['NPS'] = df['SD'] * df['NCA'] * (df['SN'] * 0.7 + 0.3)
-    df['MC'] = 1800 * df['NPS']
+    df['MC'] = 1500 * df['NPS']
     df['Mês'] = pd.to_datetime(df['Data Corte']).dt.strftime('%B')
     #df['Ano'] = pd.to_datetime(df['Data Corte']).dt.year
     
@@ -337,6 +337,7 @@ else:
     st.dataframe(df_md_pa_plot[["Iniciador", "PA", "MC", "MC_formatted"]])
 
     # st.write("Média = ", round(media_md_pa, 2))
+
 
 
 
