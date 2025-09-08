031beff47e622dcdb7caed6fcddf40e91111e266
AlysonSouza61 <alyson_anapaz61@hotmail.com>
2025-09-08 01:53:21 -0300

    Update home.py

--- home.py
+++ home.py
@@ -293,9 +293,19 @@ else:
     ####
 
 col1, col2, col3 = st.columns(3)
-
+# Coluna 1 com cor condicional
+cor_sn = "#ff4d4d" if media_sn < 0.49 else "#4CAF50"  # vermelho <0,49, verde >=0,49
 with col1:
-    st.metric(label="Média SN", value=media_sn)
+    #st.metric(label="Média SN", value=media_sn)
+    st.markdown(
+        f"""
+        <div style="background-color:{cor_sn}; padding:20px; border-radius:10px; text-align:center;">
+            <h4 style="margin:0; color:white;">Média SN</h4>
+            <p style="margin:0; font-size:24px; color:white;"><b>{media_sn:.2f}</b></p>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
 with col2:
     st.metric(label="Média NPS", value=media_nps)
@@ -425,6 +435,7 @@ regras_bonus = pd.DataFrame({
 
 st.subheader("Regras de Bônus por SN")
 st.table(regras_bonus)
+
 
 
 
