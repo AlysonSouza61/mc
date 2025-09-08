2e65445c261797f61b2b937b7475ad98923ca9cc
AlysonSouza61 <alyson_anapaz61@hotmail.com>
2025-09-07 22:52:56 -0300

    Update home.py

--- home.py
+++ home.py
@@ -342,37 +342,6 @@ else:
     # st.write("Média = ", round(media_md_pa, 2))
 
 
-     # Gráfico
-
-    df_grouped_iniciado = df.groupby("Iniciador")["SN"].mean().reset_index()
-
-    df_grouped_iniciado = df_grouped_iniciado.sort_values(by="SN", ascending=False)
-
-    # Formatar os valores de MC como moeda BR (R$)
-    df_grouped_iniciado['MC_formatted'] = df_grouped_iniciado['SN'].apply(lambda x: f'{x:,.2f}')
-
-    # Inicializando o app Dash
-    app = dash.Dash(__name__)
-    
-    # Criando o gráfico com rótulos de dados
-    fig = px.bar(df_grouped_iniciado, x='Iniciador', y='SN', title='Média do SN por Iniciador')
-
-    # Adicionando rótulos de dados no gráfico com formatação de moeda BR
-    fig.update_traces(text=df_grouped_iniciado['MC_formatted'], textposition='outside')
-
-    # Configuração do Streamlit
-    # Ajustando o layout (tamanho do gráfico)
-    fig.update_layout(
-        width=1000,  # Largura do gráfico
-        height=600,  # Altura do gráfico
-        margin=dict(t=50, b=100, l=50, r=50),  # Margens para evitar corte
-    )
-    #st.title("Média do MC por Iniciador")
-    st.plotly_chart(fig)
-        
-    # Exibir dataframe filtrado
-    st.dataframe(df)
-
 
 
 
