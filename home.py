88c42fd1a02486aae87dae8a2b5afa93728775e1
AlysonSouza61 <alyson_anapaz61@hotmail.com>
2025-09-07 23:18:06 -0300

    Update home.py

--- home.py
+++ home.py
@@ -322,21 +322,21 @@ else:
 
     # Gráfico
 
-    df_grouped_iniciado = df.groupby("Iniciador")["SN"].mean().reset_index()
+    df_grouped_iniciado_SN = df.groupby("Iniciador")["SN"].mean().reset_index()
 
-    df_grouped_iniciado = df_grouped_iniciado.sort_values(by="SN", ascending=False)
+    df_grouped_iniciado_SN = df_grouped_iniciado_SN.sort_values(by="SN", ascending=False)
 
-    # Formatar os valores de MC como moeda BR (R$)
-    df_grouped_iniciado['MC_formatted'] = df_grouped_iniciado['SN'].apply(lambda x: f'{x:,.2f}')
+    # Formatar os valores de SN
+    df_grouped_iniciado_SN['SN_formatted'] = df_grouped_iniciado_SN['SN'].apply(lambda x: f'{x:,.2f}')
 
     # Inicializando o app Dash
     app = dash.Dash(__name__)
     
     # Criando o gráfico com rótulos de dados
-    fig = px.bar(df_grouped_iniciado, x='Iniciador', y='SN', title='Média do SN por Iniciador')
+    fig = px.bar(df_grouped_iniciado_SN, x='Iniciador', y='SN', title='Média do SN por Iniciador')
 
     # Adicionando rótulos de dados no gráfico com formatação de moeda BR
-    fig.update_traces(text=df_grouped_iniciado['MC_formatted'], textposition='outside')
+    fig.update_traces(text=df_grouped_iniciado_SN['SN_formatted'], textposition='outside')
 
     # Configuração do Streamlit
     # Ajustando o layout (tamanho do gráfico)
@@ -345,11 +345,12 @@ else:
         height=600,  # Altura do gráfico
         margin=dict(t=50, b=100, l=50, r=50),  # Margens para evitar corte
     )
-    #st.title("Média do MC por Iniciador")
+    st.title("Média do SN por Iniciador")
     st.plotly_chart(fig)
         
     # Exibir dataframe filtrado
     #st.dataframe(df)
+
 
 
 
