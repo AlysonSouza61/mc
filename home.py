7e73ae4e57d4f09fdd39e30001d2846d7a9d37f1
AlysonSouza61 <alyson_anapaz61@hotmail.com>
2025-12-04 02:12:04 -0300

    Update home.py

--- home.py
+++ home.py
@@ -68,9 +68,7 @@ if uploaded_file:
     # Fator 5: (df['SN'] * 0.005 + 0.995)
     # Fator 5: (df['SN'] * 0.001 + 0.999)
     # Fator 5: (df['SN'] * 0.3 + 0.7) Estava esse
-    #df['NPS'] = df['SD'] * df['NCA'] * (df['SN'] * 0.3 + 0.7)
-    #df['NPS'] = 1 * 1 * (df['SN'] * 0.3 + 0.7)
-    df['NPS'] = df['SD'] * 1 * 1
+    df['NPS'] = df['SD'] * df['NCA'] * (df['SN'] * 0.3 + 0.7)
     df['MC'] = 1500 * df['NPS']
     df['Mês'] = pd.to_datetime(df['Data Corte']).dt.strftime('%B')
     #df['Ano'] = pd.to_datetime(df['Data Corte']).dt.year
@@ -320,6 +318,7 @@ regras_bonus = pd.DataFrame({
 
 st.subheader("Critérios de Bônus por SPS (válidas se a Média do Departamento ≥ 0,49)")
 st.table(regras_bonus)
+
 
 
 
