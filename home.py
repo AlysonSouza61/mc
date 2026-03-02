f8bca0c3c2eeaafdb33bf530170abb3b8dcf0e58
AlysonSouza61 <alyson_anapaz61@hotmail.com>
2026-03-02 20:53:58 -0300

    Update home.py

--- home.py
+++ home.py
@@ -301,7 +301,9 @@ st.plotly_chart(fig)
 
 # Exibe a tabela com os valores do novo gráfico
 #st.title("Tabela: Iniciador / PA / MC")
-st.dataframe(df)
+#st.dataframe(df[['coluna1', 'coluna2']])
+#st.dataframe(df)
+
 
 
 
