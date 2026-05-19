9438390fe435b1dc25d7708e83c81b2bf5f4799c
AlysonSouza61 <alyson_anapaz61@hotmail.com>
2026-05-19 12:01:00 -0300

    Update home.py

--- home.py
+++ home.py
@@ -72,7 +72,7 @@ if uploaded_file:
     # Fator 6: (df['SN'] * 0.75 + 0.25)
 
     # Zera SD e NCA quando SN for 0
-    # df.loc[df['SN'] == 0, ['SD', 'NCA']] = 0
+    df.loc[df['SN'] == 0, ['SD', 'NCA']] = 0
 
     df['NPS'] = df['SD'] * df['NCA'] * (df['SN'] * 0.75 + 0.25)
     df['MC'] = 1500 * df['NPS']
